b4ce0a330b63d03ad11afe25b1e34109829c85d8
Mikkel Paltorp Schmitt <mikkel.paltorp@gmail.com>
2020-11-12 11:53:36 +0100

    Update spline_kernel.jl

--- src/spline_kernel.jl
+++ src/spline_kernel.jl
@@ -19,8 +19,8 @@ function spline_kernel(t::AbstractArray, p::Int)
 
     fp = factorial.(p-1:-1:0)
     a = alpha(p).*fp;
-    Ut = (repeat(t,p,1).^Vector(p-1:-1:0))./fp;
-    Vt = (repeat(t,p,1).^Vector(p:2*p-1)).*a;
+    Ut = (repeat(t,p,1).^Vector(p-1:-1:0)')./fp';
+    Vt = (repeat(t,p,1).^Vector(p:2*p-1)').*a';
 
     return Ut,Vt;
 end
